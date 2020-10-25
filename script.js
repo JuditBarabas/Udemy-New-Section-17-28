9fe2a8b3dc80c98a6917e46413bc6fb6e6ea44dd
JuditBarabas <62601033+JuditBarabas@users.noreply.github.com>
2020-10-25 11:40:49 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log('For solution open js file');
+
+// prewritten
+const airplaneSeats = [
+    ['Ruth', 'Anthony', 'Stevie'],
+    ['Amelia', 'Pedro', 'Maya'],
+    ['Xavier', 'Ananya', 'Luis'],
+    ['Luke', null, 'Deniz'],
+    ['Rin', 'Sakura', 'Francisco']
+];
+
+// YOUR CODE GOES BELOW THIS LINE:
+airplaneSeats[3][1] = 'Hugo';
